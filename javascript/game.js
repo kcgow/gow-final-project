5700c25e0f7f9135f60108c7a600b53cc85de703
kcgow <103136933+kcgow@users.noreply.github.com>
2022-04-11 16:00:45 -0400

    hamburger menu

---
+++ javascript/game.js
@@ -0,0 +1,67 @@
+// 6 Second Timer
+const timeH = document.getElementById("seconds");
+let timeSecond = 6;
+
+const countDown = setInterval(() => {
+    timeSecond--;
+    timeH.innerHTML = timeSecond;
+
+    if (timeSecond <= 0 || timeSecond < 1) {
+        timeH.innerHTML = `Out of time!`;
+    }
+}, 1000);
+
+// Question
+const questionBox = document.getElementById('question-box');
+
+let question = [
+    "sodas",
+    "national parks",
+    "social media apps",
+    "airports",
+    "famous Toms",
+    "planets",
+    "presidents",
+    "toothpaste brands",
+    "animals that start with A",
+    "countries in South America",
+    "Canadian provinces",
+    "shades of blue",
+    "famous Sarahs",
+    "school subjects",
+    "phone companies",
+    "national pizza chains",
+    "christmas movies",
+    "farm animals",
+    "dog breeds",
+    "cat breeds",
+    "vacation destinations",
+    "types of flowers",
+    "beer brands",
+    "disney movies",
+    "reptiles",
+    "brunch orders",
+    "jewelry stores",
+    "grammy award winners",
+    "famous doctors",
+    "methods of air transportation",
+    "car brands",
+    "famous glasses-wearers",
+    "countries in Europe",
+    "fast food chains",
+    "songs",
+    "flavors of potato chips",
+    "types of exercise",
+    "things you'd find in an office",
+];
+
+let randomQuestion = question[Math.floor(Math.random() * question.length)];
+
+function supplyQuestion() {
+    questionBox.innerHTML = randomQuestion;
+}
+supplyQuestion();
+
+// Done Button
+const doneButton = document.getElementById("done-button");
+doneButton.addEventListener("click", reload, false);
--- javascript/main.js
+++ javascript/main.js
@@ -1,65 +1,16 @@
-// 6 Second Timer
-const timeH = document.getElementById("seconds");
-let timeSecond = 6;
 
-const countDown = setInterval(() => {
-    timeSecond--;
-    timeH.innerHTML = timeSecond;
+// Show Menu
+const menuButton = document.getElementById("menu")
+const menuCard = document.getElementById("menu-card")
 
-    if (timeSecond <= 0 || timeSecond < 1) {
-        timeH.innerHTML = `Out of time!`;
-    }
-}, 1000);
+menuButton.addEventListener("mouseover", showMenuCard);
+menuCard.addEventListener("mouseleave", hideMenuCard);
 
-// Question
-const questionBox = document.getElementById('question-box');
+function showMenuCard() {
+    console.log("hi");
+    menuCard.classList.add("show");
 
-let question = [
-    "sodas",
-    "national parks",
-    "social media apps",
-    "airports",
-    "famous Toms",
-    "planets",
-    "presidents",
-    "toothpaste brands",
-    "animals that start with A",
-    "countries in South America",
-    "Canadian provinces",
-    "shades of blue",
-    "famous Sarahs",
-    "school subjects",
-    "phone companies",
-    "national pizza chains",
-    "christmas movies",
-    "farm animals",
-    "dog breeds",
-    "cat breeds",
-    "vacation destinations",
-    "types of flowers",
-    "beer brands",
-    "disney movies",
-    "reptiles",
-    "brunch orders",
-    "jewelry stores",
-    "grammy award winners",
-    "famous doctors",
-    "methods of air transportation",
-    "car brands",
-    "famous glasses-wearers",
-];
-
-let randomQuestion = question[Math.floor(Math.random() * question.length)];
-
-function supplyQuestion() {
-    questionBox.innerHTML = randomQuestion;
 }
-
-supplyQuestion();
-
-// Done Button
-const doneButton = document.getElementById("done-button");
-doneButton.addEventListener("click", reload, false);
-
-
-
+function hideMenuCard() {
+    menuCard.classList.remove("show");
+}
